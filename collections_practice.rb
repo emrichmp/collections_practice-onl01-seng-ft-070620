0a3fb88689430261b5ac300e4a8d2185af585af9
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-10 21:58:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,15 @@ def sort_array_asc(array)
     a <=> b
   end
 end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+  end
+  end
+end
